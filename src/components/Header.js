783e36eb050ae39205b0ec1041ef9506916a3905
Vadim Smagin <magenvadim@gmail.com>
2023-12-08 23:57:06 +0200

    #5 Header css style has added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,13 @@ function Header() {
   return (
     <>
       <header className="header">
-        <h1>Where in the world!</h1>
+        <div>
+          <h1>Where in the world ?</h1>
+        </div>
+        <div>
+          <i className="fas fa-moon"></i>Dark mode
+        </div>
+        
       </header>
     </>
   )
