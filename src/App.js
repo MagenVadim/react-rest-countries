b607b60e2e1b48429354a0db79a2871001d3ee82
Vadim Smagin <magenvadim@gmail.com>
2023-12-09 04:10:56 +0200

    #7 Filter component has created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Countries from './components/Countries';
 import Header from './components/Header';
+import Filter from './components/Filter';
 
 function App() {
   return (
     <>
       <Header/>
+      <Filter/>
       <Countries/>      
     </>
   );
