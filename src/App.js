86ac38fee4c620f5da30988a6a17913fa8e87c08
Vadim Smagin <magenvadim@gmail.com>
2023-12-10 01:17:17 +0200

    #9 CSS adaptive was added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Countries from './components/Countries';
 import Header from './components/Header';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Filter from './components/Filter';
 
 function App() {
