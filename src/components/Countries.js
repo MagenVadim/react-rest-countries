e5a0cdf1e9ade3bf540fc2b638c85c75ae2222f9
Vadim Smagin <magenvadim@gmail.com>
2023-12-17 03:45:48 +0200

    #18 Filter varuables was added

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -32,7 +32,7 @@ const Countries = () => {
                   <div className="country-wrap">
                     <img src={flag }alt={name} />
                     <div className="details">
-                      <h3>{name}</h3>
+                      <h3 className="country-name">{name}</h3>
                       <h4>Population: <span>{population}</span> </h4>
                       <h4>Region: <span>{region}</span></h4>
                       <h4>Capital: <span>{capital}</span> </h4>  
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 const Filter = () => {
+  
+  const inputChangeHandlrt = (e)=>{
+    const {value} = e.target
+    const countryName = document.querySelectorAll('.country-name')
+
+    countryName.forEach((name)=>{
+      console.log(name.innerText)
+    })
+  } 
+  
+
   return (
     <section className="filter">
         <form className="form-control">
-            <input className="input" type="search" name="search" id="search" placeholder='Search for a country'/>
+            <input className="input" type="search" name="search" id="search" placeholder='Search for a country' onChange={inputChangeHandlrt}/>
         </form>
         <div className="region-filter">
             <select name="select" id="select" className="select">
